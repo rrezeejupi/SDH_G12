5d910f4c1ebd919f41186fe26a4300cf2093cc0c
Rreze Ejupi <rrezeejupi@gmail.com>
2025-04-19 13:56:57 +0200

    Beaufort Cipher encryption

--- src/main/java/BeaufortCipher.java
+++ src/main/java/BeaufortCipher.java
@@ -1,6 +1,27 @@
 public class BeaufortCipher {
-    public static void main(String[]args){
 
+    public static String encrypt(String plaintext, String key) {
+        StringBuilder ciphertext = new StringBuilder();
+        plaintext = plaintext.toUpperCase();
+        key = key.toUpperCase();
+
+        int keyIndex = 0;
+        for (int i = 0; i < plaintext.length(); i++) {
+            char p = plaintext.charAt(i);
+
+            if (Character.isLetter(p)) {
+                char k = key.charAt(keyIndex % key.length());
+
+                int c = (k - p + 26) % 26;
+                char cipherChar = (char) ('A' + c);
+                ciphertext.append(cipherChar);
+
+                keyIndex++;
+            } else {
+                ciphertext.append(p);
+            }
+        }
+
+        return ciphertext.toString();
     }
 }
-
